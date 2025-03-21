40e4f308757cd695be42fe47756147a6c9e63c56
roggoz <reaganroggoz@gmail.com>
2025-03-21 11:57:02 +0300

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,24 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-
 // routes
 app.use("/api/auth", require("./routes/authRoutes"));
 
+mongoose
+  .connect(process.env.VITE_API_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-
-mongoose.connect(process.env.VITE_API_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log(err));
-
-
-const PORT = process.env.PORT  || 5000;
-app.listen(PORT, ()=> {
-    console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-
